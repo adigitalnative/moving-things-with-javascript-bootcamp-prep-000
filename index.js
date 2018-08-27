0577d1da140c063c98a0c78539f68a302cdf4055
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 19:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = "I don't do much."
 function moveDodgerRight() {
     var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
-    const leftLimit = parseInt(dodger.)
     
-    if (left < 400 - parseInt(dodger.style.left)) {
+    if (left < 360 {
         dodger.style.left = `${left + 1}px`
     }
 }
