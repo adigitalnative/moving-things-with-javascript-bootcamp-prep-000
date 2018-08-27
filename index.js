df1665c9910eaa37797f78d19ad503262a9a846d
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 19:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const app = "I don't do much."
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
 
 function moveDodgerRight() {
     var leftNumbers = dodger.style.left.replace('px', '')
